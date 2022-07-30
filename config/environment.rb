d7122e30936041ca29f167a037b70a89026e8491
iAcay <96024046+iAcay@users.noreply.github.com>
2022-07-31 00:18:55 +0200

    feat: introduce sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = { 
+  user_name: Rails.application.credentials.dig(:sendgrid, :user_name)
+  password: Rails.application.credentials.dig(:sendgrid, :password)
+  domain: 'management-app-by-acay.herokuapp.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+ }
