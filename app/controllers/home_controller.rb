e606f2825b797364814548c503282a09107362d9
iAcay <96024046+iAcay@users.noreply.github.com>
2022-10-08 12:57:59 +0200

    fix: frontend changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @projects = (ActsAsTenant.current_tenant.present? ? ActsAsTenant.current_tenant.projects : []) 
+    projects = (ActsAsTenant.current_tenant.present? ? ActsAsTenant.current_tenant.projects : []) 
+    render :index, locals: { projects: projects }
   end
 end
