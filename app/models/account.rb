30d4dfe93173adcab75e537ede9a97fc9af77fac
iAcay <96024046+iAcay@users.noreply.github.com>
2022-10-12 20:47:44 +0200

    feat: add admin to accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,7 @@
 class Account < ApplicationRecord
   has_many :users, dependent: :nullify
   has_many :projects, dependent: :destroy
+  belongs_to :admin, class_name: 'User', inverse_of: :administered_account
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   enum plan: { free: 0, premium: 1 }, _prefix: true
